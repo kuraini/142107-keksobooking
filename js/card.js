bb685752ac9e78f341a66d29c31c4402e64a0f1c
Svetlana Borisova <kuraini@yandex.ru>
2017-12-12 01:20:19 +0300

    Добавлена функция giveOfferType и переменная offer

--- js/card.js
+++ js/card.js
@@ -3,18 +3,14 @@
 (function () {
   var mapCardTemplate = document.querySelector('template').content.querySelector('.map__card');
 
-  function getOfferType(card) {
-    var offerType;
+  function giveOfferType(card) {
     if (card.offer.type === 'flat') {
-      offerType = 'Квартира';
+      return 'Квартира';
+    } else if (card.offer.type === 'house') {
+      return 'Дом';
+    } else {
+      return 'Бунгало';
     }
-    if (card.offer.type === 'house') {
-      offerType = 'Дом';
-    }
-    if (card.offer.type === 'bungalo') {
-      offerType = 'Бунгало';
-    }
-    return offerType;
   }
 
   function renderCard(card) {
@@ -29,6 +25,7 @@
     var featureItems = cardElement.querySelectorAll('.feature');
     var description = cardElement.querySelector('.popup__pictures').previousElementSibling;
     var avatar = cardElement.querySelector('.popup__avatar');
+    var offer = card.offer;
 
     function removeItem() {
       for (var i = 0; i < featureItems.length; i++) {
@@ -38,25 +35,25 @@
     }
 
     function makeItem() {
-      for (var i = 0; i < card.offer.features.length; i++) {
+      for (var i = 0; i < offer.features.length; i++) {
         var item = document.createElement('li');
-        item.setAttribute('class', 'feature feature--' + card.offer.features[i]);
+        item.setAttribute('class', 'feature feature--' + offer.features[i]);
         featuresList.appendChild(item);
       }
     }
 
-    header.textContent = card.offer.title;
-    address.textContent = card.offer.address;
-    price.innerHTML = card.offer.price + '&#x20bd;/ночь';
-    type.textContent = getOfferType(card);
-    roomAndGuest.textContent = card.offer.rooms + ' ' +
-                              window.util.doDeclension(card.offer.rooms, ['комната', 'комнаты', 'комнат']) +
-                              ' для ' + card.offer.guests + ' ' +
-                              window.util.doDeclension(card.offer.guests, ['гостя', 'гостей', 'гостей']);
-    checkinAndCheckout.textContent = 'Заезд после ' + card.offer.checkin + ', выезд до ' + card.offer.checkout;
+    header.textContent = offer.title;
+    address.textContent = offer.address;
+    price.innerHTML = offer.price + '&#x20bd;/ночь';
+    type.textContent = giveOfferType(card);
+    roomAndGuest.textContent = offer.rooms + ' ' +
+                              window.util.doDeclension(offer.rooms, ['комната', 'комнаты', 'комнат']) +
+                              ' для ' + offer.guests + ' ' +
+                              window.util.doDeclension(offer.guests, ['гостя', 'гостей', 'гостей']);
+    checkinAndCheckout.textContent = 'Заезд после ' + offer.checkin + ', выезд до ' + offer.checkout;
     removeItem();
     makeItem();
-    description.textContent = card.offer.description;
+    description.textContent = offer.description;
     avatar.setAttribute('src', card.author.avatar);
     cardElement.classList.add('hidden');
 
