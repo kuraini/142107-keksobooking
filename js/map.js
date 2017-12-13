f44dd2acba80beee40ca2b8af290b51161168c38
Svetlana Borisova <kuraini@yandex.ru>
2017-12-13 07:52:49 +0300

    Добавлена реакция на перемещение .map__pin--main и передача value для поля адреса в форме

--- js/map.js
+++ js/map.js
@@ -1,6 +1,13 @@
 'use strict';
 
 (function () {
+  var MAIN_PIN_HEIGHT = 65;
+  var MAIN_PIN_PSEUDO_HEIGHT = 22;
+  var MAIN_PIN_HEIGHT_IN_PROCENT = 100;
+  var MAIN_PIN_TRANSITION = Math.floor((MAIN_PIN_PSEUDO_HEIGHT * 100 / MAIN_PIN_HEIGHT) + MAIN_PIN_HEIGHT_IN_PROCENT);
+  var MAP_TOP_LIMIT = 100;
+  var MAP_BOTTOM_LIMIT = 500;
+
   window.advertisments = window.makeAd(8);
 
   var mapPinsContainer = document.querySelector('.map__pins');
@@ -91,4 +98,52 @@
   }
 
   mapPinsContainer.addEventListener('click', onMapPinClick);
+
+  var inputAddress = form.querySelector('#address');
+
+  mapPinMain.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    function onMouseMove(moveEvt) {
+      moveEvt.preventDefault();
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      var shiftCoords = {
+        x: mapPinMain.offsetLeft - shift.x,
+        y: mapPinMain.offsetTop - shift.y
+      };
+
+      if ((shiftCoords.y >= MAP_TOP_LIMIT) && (shiftCoords.y <= MAP_BOTTOM_LIMIT)) {
+        mapPinMain.setAttribute('style', 'top: ' + shiftCoords.y + 'px; ' +
+                                'left: ' + shiftCoords.x + 'px; ' +
+                                'transform: translate(-50%, -' + MAIN_PIN_TRANSITION + '%);');
+      }
+
+      inputAddress.setAttribute('value', 'x: ' + shiftCoords.x + ', y: ' + shiftCoords.y);
+    }
+
+    function onMouseUp(upEvt) {
+      upEvt.preventDefault();
+
+      map.removeEventListener('mousemove', onMouseMove);
+      map.removeEventListener('mouseup', onMouseUp);
+    }
+
+    map.addEventListener('mousemove', onMouseMove);
+    map.addEventListener('mouseup', onMouseUp);
+  });
 })();
