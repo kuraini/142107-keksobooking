b12543c0928fee588e908e24842fefb6696a76cf
Svetlana Borisova <kuraini@yandex.ru>
2017-12-16 16:57:18 +0300

    Исправлен модуль show-card.js

--- js/map.js
+++ js/map.js
@@ -18,7 +18,6 @@
   var map = document.querySelector('.map');
   map.insertBefore(window.makeCard(window.advertisments[0]), filtersContainer);
 
-
   var mapPinMain = map.querySelector('.map__pin--main');
   var form = document.querySelector('.notice__form');
   var fieldsets = form.querySelectorAll('fieldset');
@@ -37,7 +36,61 @@
 
   mapPinMain.addEventListener('mouseup', onMapPinMainMouseup);
 
-  window.showCard();
+  var selectedPin;
+
+  function getClassActive(node) {
+    if (selectedPin) {
+      selectedPin.classList.remove('map__pin--active');
+    }
+    selectedPin = node;
+    selectedPin.classList.add('map__pin--active');
+  }
+
+  function onMapPinClick(evt) {
+    var target = evt.target.closest('.map__pin');
+
+    if (!target || target.classList.contains('map__pin--main')) {
+      return;
+    }
+
+    getClassActive(target);
+
+    var pinId = Number(target.getAttribute('id'));
+
+    window.showCard(pinId);
+
+    var popup = map.querySelector('.popup');
+    var popupClose = popup.querySelector('.popup__close');
+
+    function onPopupEscPress(e) {
+      window.util.isEscEvent(e, closePopup);
+    }
+
+    function openPopup() {
+      window.showCard(pinId);
+      document.addEventListener('keydown', onPopupEscPress);
+    }
+
+    function closePopup() {
+      map.removeChild(popup);
+      target.classList.remove('map__pin--active');
+      document.removeEventListener('keydown', onPopupEscPress);
+    }
+
+    popupClose.addEventListener('click', closePopup);
+
+    popupClose.addEventListener('keydown', function (e) {
+      window.util.isEnterEvent(e, closePopup);
+    });
+
+    target.addEventListener('keydown', function (e) {
+      window.util.isEnterEvent(e, openPopup);
+    });
+
+    document.addEventListener('keydown', onPopupEscPress);
+  }
+
+  mapPinsContainer.addEventListener('click', onMapPinClick);
 
   var inputAddress = form.querySelector('#address');
 
